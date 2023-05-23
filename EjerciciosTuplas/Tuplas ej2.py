97e1d80d3a0c6619b4dc50c8107df440d3523a88
Agustina Aperlo <43002670+magustinaperlo@users.noreply.github.com>
2023-05-23 18:08:27 -0300

    Update_Tuplas ej2.py

--- EjerciciosTuplas/Tuplas ej2.py
+++ EjerciciosTuplas/Tuplas ej2.py
@@ -8,3 +8,19 @@ numeros = (1, 2, 2, 3, 4, 9, 7, 6, 3, 2, 6, 6, 2, 3, 4, 2, 4, 6, 4, 6, 3, 7 ,6, 
 while True:
     num = int(input("Ingrese numero: "))
     print(numeros.count(num))    
+
+    #sugerencia 
+    
+    numeros = (1, 2, 2, 3, 4, 9, 7, 6, 3, 2, 6, 6, 2, 3, 4, 2, 4, 6, 4, 6, 3, 7, 6, 7, 8, 9, 10, 11, 1, 10, 10)
+
+while True:
+    try:
+        num = int(input("Ingrese un número (0 para salir): "))
+        
+        if num == 0:
+            break
+        
+        count = numeros.count(num)
+        print(f"El número {num} se repite {count} veces en la tupla.")
+    except ValueError:
+        print("Error: Ingrese un número válido.")
